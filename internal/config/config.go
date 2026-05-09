c93ef488fe5d19b446575d5b592f4d94170a9fc9
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-05-09 13:47:28 +0300

    refactoring

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,6 @@
 package config
 
-import (
-	"errors"
-	"os"
-)
+import "os"
 
 type Config struct {
 	Port        string
@@ -11,15 +8,10 @@ type Config struct {
 	LogLevel    string
 }
 
-func Load() (Config, error) {
+func Load() Config {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-	}
-
-	databaseURL := os.Getenv("DATABASE_URL")
-	if databaseURL == "" {
-		return Config{}, errors.New("DATABASE_URL is required")
 	}
 
 	logLevel := os.Getenv("LOG_LEVEL")
@@ -29,7 +21,7 @@ func Load() (Config, error) {
 
 	return Config{
 		Port:        port,
-		DatabaseURL: databaseURL,
+		DatabaseURL: os.Getenv("DATABASE_URL"),
 		LogLevel:    logLevel,
-	}, nil
+	}
 }
